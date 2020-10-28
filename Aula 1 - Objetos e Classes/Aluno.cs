743c15020d87bb2e870edb678386489aa2143d47
William Chenta <william.chenta@gmail.com>
2020-10-28 20:48:26 -0300

    melhorias

---
+++ Aula 1 - Objetos e Classes/Aluno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Objetos_e_Classes
+{
+    class Aluno
+    {
+        public int matricula { get; set; }
+    }
+}
