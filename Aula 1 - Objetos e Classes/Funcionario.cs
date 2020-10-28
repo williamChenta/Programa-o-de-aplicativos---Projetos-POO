eadbb43f31e30d7d47349a6dc64e8bf9c894f636
William Chenta <william.chenta@gmail.com>
2020-10-28 20:09:35 -0300

    iniciado o versionamento do projeto e alterado a classe funcionario. mensagem de erro da validação da idade do método calcularAumento

---
+++ Aula 1 - Objetos e Classes/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Objetos_e_Classes
+{
+    public class Funcionario
+    {
+        public string nome;
+        public string email;
+        public int idade;
+        public string telefone;
+        public double salario;
+        public string cargo;
+
+        public void imprimirFicha()
+        {
+            Console.WriteLine($"Nome: {nome}\nEmail: {email}\nIdade: {idade}");
+        }
+        public void calcularAumento()
+        {
+            if (idade > 30)
+            {
+                salario = salario * 1.15;
+                Console.WriteLine($"seu novo salario é: R${salario}");
+            } else
+            {
+                Console.WriteLine("Você não atingiu a idade necessária ainda!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {nome}\nEmail: {email}\nIdade: {idade}";
+        }
+    }
+}
---
+++ Aula 1 - Objetos e Classes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Objetos_e_Classes
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            //william e golle são objetos da classe Funcionario
+            Funcionario william = new Funcionario();
+            william.nome = "william chenta";
+            william.idade = 44;
+            william.salario = 1000;
+
+            Console.WriteLine(william);
+
+
+            Funcionario nasatto = new Funcionario()
+            {
+                cargo = "Coordenador",
+                email = "nasatto@gmail.com",
+                telefone = "4444-4444"
+            };
+
+            //nasatto.idade = 25;
+            //nasatto.telefone = "5555-5555";
+
+
+            ////Console.WriteLine($"o telefone do {william.nome} é: {william.telefone}");
+
+            ////william.imprimirFicha();
+            //william.calcularAumento();
+
+            ////Funcionario golle = new Funcionario();
+            ////golle.nome = "jorge golle";
+            ////golle.idade = 40;
+            ////golle.telefone = "444-4444";
+
+            ////Console.WriteLine($"Deseja imprimir a idade do {william.nome} ?");
+            //if (bool.Parse(Console.ReadLine()))
+            //{
+            //    Console.WriteLine($"a idade do {william.nome} é: {william.idade}");
+            //}
+        }
+    }
+}
